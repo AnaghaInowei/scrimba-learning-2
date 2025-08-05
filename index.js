bd610487bcf5c691404a56c2b1fecaf37df02149
Ino Tech <ezekieldavid150@gmail.com>
2025-08-05 21:23:31 +0100

    scripting

---
+++ index.js
@@ -0,0 +1,5 @@
+//fetching the innertext of the scoreboard
+let score = document.querySelector('.score').textContent;
+
+//fetching the innertext of the button
+let buttonText = document.querySelector('.button').textContent; 
