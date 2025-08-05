3b7d6414f596ad24ce5f15e84bb30ec741c8dac0
Ino Tech <ezekieldavid150@gmail.com>
2025-08-05 19:53:11 +0100

    practice

---
+++ index.js
@@ -0,0 +1,14 @@
+const firstName = "Inowei";
+const lastName = "Anagha";
+
+console.log(`Hello, my name is ${firstName} ${lastName}.`);
+
+
+let name = "Linda";
+let greeting = "Ohayo";
+
+function greet() {
+    console.log(`${greeting}, ${name}-san!`);
+}
+
+greet();
