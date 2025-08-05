1bfdf2297d66235ae3121a5c13c4298c169b1ba3
Ino Tech <ezekieldavid150@gmail.com>
2025-08-05 21:40:46 +0100

    scripting

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 //fetching the innertext of the scoreboard
-let score = document.querySelector('.score').textContent;
+let score = document.getElementById('score');
 
 //fetching the innertext of the button
-let buttonText = document.querySelector('.button').textContent; 
+let buttonText = document.getElementsByClassName('btn');
+
+let scoreNum = 0;
+
+function increment1() {
+  //incrementing the score by 1
+  score.textContent = parseInt(score.innerText) + 1; 
+}
